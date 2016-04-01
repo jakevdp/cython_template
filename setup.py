3a9b02d599afcbbad668829e7daf8da41b64659c
Jake VanderPlas <jakevdp@gmail.com>
2016-04-01 15:01:40 -0700

    clean up setup.py script

--- setup.py
+++ setup.py
@@ -17,27 +17,23 @@ DOWNLOAD_URL = 'https://github.com/jakevdp/cython_template'
 LICENSE = 'BSD 2-clause'
 
 
-def read(path, encoding='utf-8'):
-    path = os.path.join(os.path.dirname(__file__), path)
-    with io.open(path, encoding=encoding) as fp:
-        return fp.read()
-
-
-def version(package):
+def version(package, encoding='utf-8'):
     """Obtain the packge version from a python file e.g. pkg/__init__.py
 
     See <https://packaging.python.org/en/latest/single_source_version.html>.
     """
-    version_file = read(os.path.join(package, '__init__.py'))
-    print(version_file)
+    path = os.path.join(os.path.dirname(__file__), package, '__init__.py')
+    with io.open(path, encoding=encoding) as fp:
+        version_info = fp.read()
     version_match = re.search(r"""^__version__ = ['"]([^'"]*)['"]""",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
+                              version_info, re.M)
+    if not version_match:
+        raise RuntimeError("Unable to find version string.")
+    return version_match.group(1)
 
 
 def generate_cython(package):
+    """Cythonize all sources in the package"""
     cwd = os.path.abspath(os.path.dirname(__file__))
     print("Cythonizing sources")
     p = subprocess.call([sys.executable,
